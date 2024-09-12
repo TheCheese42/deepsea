3c52fa38f2f0cf0829aa65c80f9d9a16b140bd5c
Dominik <dominik.reinartz@proton.me>
2024-09-12 12:51:41 +0200

    Basic outline

--- deepsea/deepsea.py
+++ deepsea/deepsea.py
@@ -1,8 +1,29 @@
-#!/usr/bin/env python3
+import time
 
-def main() -> None:
+try:
+    from .model import DeepObject
+except ImportError:
+    from model import DeepObject
+
+
+def full_update(delta: float) -> list[DeepObject]:
     pass
 
 
-if __name__ == "__main__":
-    main()
+def full_draw(objects: list[DeepObject]) -> None:
+    pass
+
+
+def main(fps: int) -> None:
+    target_delta = 1 / fps
+    time_before_last_update = 0.0
+    time_after_last_update = 0.0
+    while True:
+        time.sleep(
+            min(target_delta - (time.time() - time_before_last_update), 0)
+        )
+        time_before_last_update = time.time()
+        all_objects = full_update(time.time() - time_after_last_update)
+        time_after_last_update = time.time()
+        full_draw(all_objects)
+        time.sleep()
